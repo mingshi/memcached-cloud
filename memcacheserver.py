244ecb5375444a0788707ffe3fbdedd5a8eef3df
孙志文 <zhiwensun@anjuke.com>
2012-05-09 15:49:13 +0800

    fix bug of zero

--- memcacheserver.py
+++ memcacheserver.py
@@ -1,23 +1,23 @@
-
-
 memcache_servers = {}
 
-memcache_servers[1] = {'group':'anjuke-prop', 'addr': '10.10.3.2:11211'  }
-memcache_servers[2] = {'group':'anjuke-prop', 'addr': '10.10.3.3:11211'  }
-memcache_servers[3] = {'group':'anjuke-prop', 'addr': '10.10.3.12:11211' }
-memcache_servers[4] = {'group':'anjuke-prop', 'addr': '10.10.3.13:11211' }
-memcache_servers[5] = {'group':'anjuke-prop', 'addr': '10.10.6.151:11211'}
-memcache_servers[6] = {'group':'anjuke-prop', 'addr': '10.10.3.33:11211' }
-memcache_servers[7] = {'group':'anjuke-prop', 'addr': '10.10.3.34:11211' }
-memcache_servers[8] = {'group':'anjuke-prop', 'addr': '10.10.3.37:11211' }
-memcache_servers[9] = {'group':'anjuke-prop', 'addr': '10.10.3.38:11211' }
-memcache_servers[10] = {'group' : 'anjuke-prop', 'addr' : '10.10.3.41:11211' }
-memcache_servers[11] = {'group' : 'anjuke-prop', 'addr' : '10.10.3.40:11211' }
-memcache_servers[12] = {'group' : 'anjuke-prop', 'addr' : '10.10.3.48:11211' }
-memcache_servers[13] = {'group' : 'group-bbs', 'addr' : '10.10.3.24:11211' }
-memcache_servers[14] = {'group' : 'localhost', 'addr' : 'localhost:11211' }
-memcache_servers[15] = {'group' : 'localhost', 'addr' : '192.168.1.96:11211' }
-memcache_servers[16] = {'group' : 'image-service', 'addr' : '10.0.1.124:11211' }
-memcache_servers[17] = {'group' : 'image-service', 'addr' : '10.0.1.125:11211' }
-memcache_servers[18] = {'group' : 'image-service', 'addr' : '10.0.1.130:11211' }
+i = 0
+i=i+1;memcache_servers[i] = {'group':'anjuke-prop', 'addr':'10.10.3.2:11211'  } 
+i=i+1;memcache_servers[i] = {'group':'anjuke-prop', 'addr':'10.10.3.3:11211'  }
+i=i+1;memcache_servers[i] = {'group':'anjuke-prop', 'addr':'10.10.3.12:11211' }
+i=i+1;memcache_servers[i] = {'group':'anjuke-prop', 'addr':'10.10.3.13:11211' }
+i=i+1;memcache_servers[i] = {'group':'anjuke-prop', 'addr':'10.10.6.151:11211'}
+i=i+1;memcache_servers[i] = {'group':'anjuke-prop', 'addr':'10.10.3.33:11211' }
+i=i+1;memcache_servers[i] = {'group':'anjuke-prop', 'addr':'10.10.3.34:11211' }
+i=i+1;memcache_servers[i] = {'group':'anjuke-prop', 'addr':'10.10.3.37:11211' }
+i=i+1;memcache_servers[i] = {'group':'anjuke-prop', 'addr':'10.10.3.38:11211' }
+i=i+1;memcache_servers[i] = {'group':'anjuke-prop', 'addr':'10.10.3.41:11211' }
+i=i+1;memcache_servers[i] = {'group':'anjuke-prop', 'addr':'10.10.3.40:11211' }
+i=i+1;memcache_servers[i] = {'group':'anjuke-prop', 'addr':'10.10.3.48:11211' }
+i=i+1;memcache_servers[i] = {'group':'group-bbs', 'addr':'10.10.3.24:11211' }
+i=i+1;memcache_servers[i] = {'group':'localhost', 'addr':'localhost:11211' }
+i=i+1;memcache_servers[i] = {'group':'localhost', 'addr':'192.168.1.96:11211' }
+i=i+1;memcache_servers[i] = {'group':'image-service', 'addr':'10.0.1.124:11211' }
+i=i+1;memcache_servers[i] = {'group':'image-service', 'addr':'10.0.1.125:11211' }
+i=i+1;memcache_servers[i] = {'group':'image-service', 'addr':'10.0.1.130:11211' }
+i=i+1;memcache_servers[i] = {'group':'anjuke-prop', 'addr':'10.10.3.31:11211' }
 
