8b0c81e0e32792508dabbc9453223e1573e47530
施明 <mingshi@anjuke.com>
2012-05-17 09:54:33 +0700

    push run.py

--- run.py
+++ run.py
@@ -1,7 +1,16 @@
 if __name__ == "__main__":
+#    from mc import app
+#
+#    if app.config['DEBUG'] :
+#        print app.config
+#
+#    app.run(host=app.config['HOST'], port=app.config['PORT'])
+
+    from tornado.wsgi import WSGIContainer
+    from tornado.httpserver import HTTPServer
+    from tornado.ioloop import IOLoop
     from mc import app
 
-    if app.config['DEBUG'] :
-        print app.config
-
-    app.run(host=app.config['HOST'], port=app.config['PORT'])
+    http_server = HTTPServer(WSGIContainer(app))
+    http_server.listen(5000)
+    IOLoop.instance().start()
