d02626da491b355b2a625fca5853aaf9c9f70c5c
施明 <mingshi@anjuke.com>
2012-06-04 10:14:10 +0700

    fix mysql engine connect bug

--- mc/db/db.py
+++ mc/db/db.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from mc import app
 
-engine = create_engine(app.config['DB_URI'], convert_unicode=True, pool_size=100, pool_recycle=7200
+engine = create_engine(app.config['DB_URI'], pool_size=100, pool_recycle=7200, convert_unicode=True, pool_size=100,
         **app.config['DB_CONNECT_OPTIONS'])
 
 db_session = scoped_session(sessionmaker(autoflush=True, bind=engine))
